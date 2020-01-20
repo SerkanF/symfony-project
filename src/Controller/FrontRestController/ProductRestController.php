e5dec757e3fd5c89266509b69002cc16d74bbabe
FIDAN Serkan <serkanfidan70@gmail.com>
2020-01-20 19:00:12 +0100

    create product entity

---
+++ src/Controller/FrontRestController/ProductRestController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller\FrontRestController;
+
+use App\Controller\AbfFrontAbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use App\Services\Logger\AbfLoggerService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use function MongoDB\BSON\toJSON;
+
+/**
+ * Class ProductRestController
+ * @package App\RestController
+ * @Route(path="/api")
+ */
+class ProductRestController extends AbfFrontAbstractController {
+
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+
+    public function __construct(AbfLoggerService $logger, ProductRepository $productRepository)
+    {
+        parent::__construct($logger);
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @Route(path="/products", methods={"GET"})
+     * @param Serializer $serializer
+     * @return JsonResponse
+     */
+    public function getProducts() : JsonResponse {
+
+        $products = $this->productRepository->findAll();
+
+        $response = new JsonResponse();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setData($products);
+
+        return $response;
+    }
+
+    /**
+     * @Route(path="/products", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createProduct(Request $request) : JsonResponse {
+
+        $data = json_decode($request->getContent(), true);
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setDescription($data['description']);
+        $product->setPrice($data['price']);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        $response = new JsonResponse();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setData($product);
+
+        return $response;
+
+    }
+
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product implements \JsonSerializable {
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'id'           => $this->id,
+            'name'        => $this->name,
+            'description' => $this->description,
+            'price'  => $this->price,
+        ];
+    }
+}
